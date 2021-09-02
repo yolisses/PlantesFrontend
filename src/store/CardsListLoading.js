02f2159d7f8c538918eb6a4464eaf19b046295dc
Ulisses <m.stunik@gmail.com>
2021-09-02 07:53:34 -0300

    remove scroll indicator from loading list

--- src/store/CardsListLoading.js
+++ src/store/CardsListLoading.js
@@ -5,6 +5,7 @@ import {Card} from 'store/Card';
 export function CardsListLoading() {
   return (
     <FlatList
+      showsVerticalScrollIndicator={false}
       data={Object.keys([...Array(8)])}
       numColumns={2}
       renderItem={item => <Card key={item} />}
