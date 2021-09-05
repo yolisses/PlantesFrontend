dc935ef15425a716b557ef306e1f5529019a5876
Ulisses <m.stunik@gmail.com>
2021-09-05 11:03:40 -0300

    use item instead of uri in user round image

--- src/user/UserInfo.js
+++ src/user/UserInfo.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {api} from 'api';
-import {LightButton} from 'common/LightButton';
 
-export function UserInfo({height}) {
+export function UserInfo({item}) {
   const [user, setUser] = useState(null);
 
   async function getUser() {
@@ -16,45 +15,20 @@ export function UserInfo({height}) {
   }, []);
 
   return (
-    <View style={[styles.container, {height}]}>
+    <View>
       <View style={styles.imageNameWrapper}>
         <FastImage style={styles.image} source={{uri: user?.image}} />
         <Text style={styles.text}>{user?.name}</Text>
-      </View>
-      <View style={styles.buttonsWrapper}>
-        <LightButton text="Seguir" style={styles.button} />
-        <LightButton text="Mensagem" style={styles.button} emphasis />
       </View>
     </View>
   );
 }
 
-const {width} = Dimensions.get('window');
-
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    width,
-  },
   imageNameWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
     padding: 10,
-  },
-  buttonsWrapper: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-evenly',
-    paddingHorizontal: 4,
-  },
-  button: {
-    flex: 1,
-    margin: 4,
-    padding: 25,
-  },
-  buttonEmphasis: {
-    flex: 1,
-    padding: 25,
   },
   text: {
     fontSize: 20,
