ef6151ed2bf25c236270b08098cf879b2ed55f72
Ulisses <m.stunik@gmail.com>
2021-09-15 14:36:39 -0300

    refatorate

--- src/camera/CameraOptionButton.js
+++ src/camera/CameraOptionButton.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {Image, TouchableOpacity} from 'react-native';
 
 export function CameraOptionButton({image, ...rest}) {
-  const size = 20;
   return (
     <TouchableOpacity {...rest} activeOpacity={0.7}>
-      <Image
-        source={image}
-        color="white"
-        style={{
-          margin: 20,
-          height: size,
-          width: size,
-        }}
-      />
+      <Image source={image} color="white" style={styles.image} />
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    margin: 20,
+    height: 20,
+    aspectRatio: 1,
+  },
+});
