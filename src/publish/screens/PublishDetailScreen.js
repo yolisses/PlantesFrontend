8e8b0362eb55728498573545b55f987678156ee0
Ulisses <m.stunik@gmail.com>
2021-09-15 08:54:36 -0300

    remove usememo from name input on detail screen

--- src/publish/screens/PublishDetailScreen.js
+++ src/publish/screens/PublishDetailScreen.js
@@ -1,5 +1,5 @@
-import React, {useCallback, useMemo} from 'react';
-import {ScrollView, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
 
 import {TextInput} from 'form/TextInput';
 import {TagsSelector} from 'form/TagsSelector';
@@ -29,17 +29,12 @@ export function PublishDetailScreen() {
   const {description, setDescription} = usePublish();
   const {tags: selectedTags, pushTag, removeTag} = usePublish();
 
-  const nameInput = useMemo(
-    () => <TextInput autoFocus label="Nome" value={name} setValue={setName} />,
-    [name, setName],
-  );
-
   return (
     <>
       <ValidatedHeader />
       <ProgressBar ratio={2 / 3} />
       <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-        {nameInput}
+        <TextInput autoFocus label="Nome" value={name} setValue={setName} />
         <SingleOptionSelector
           label="Marcar como"
           value={type}
