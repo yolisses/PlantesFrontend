f4ac39374513e3b9fb224d6c252bcd6f85acb5de
Ulisses <m.stunik@gmail.com>
2021-09-16 09:31:25 -0300

    reset routes

--- src/publish/screens/PublishScreen.js
+++ src/publish/screens/PublishScreen.js
@@ -11,7 +11,7 @@ const Publish = createNativeStackNavigator();
 export function PublishScreen() {
   return (
     <Publish.Navigator>
-      {/* <Publish.Screen
+      <Publish.Screen
         name="Images"
         component={PublishImagesScreen}
         options={{headerShown: false}}
@@ -20,7 +20,7 @@ export function PublishScreen() {
         name="Detail"
         component={PublishDetailScreen}
         options={{headerShown: false}}
-      /> */}
+      />
       <Publish.Screen
         name="Price"
         component={PublishPriceScreen}
