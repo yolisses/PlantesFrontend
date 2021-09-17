6c72a3a4a4ea1818c3c513de0d350fdb1f7df1ab
Ulisses <m.stunik@gmail.com>
2021-09-17 11:12:59 -0300

    add header validation on publish images screen

--- src/publish/screens/PublishImagesScreen.js
+++ src/publish/screens/PublishImagesScreen.js
@@ -1,37 +1,112 @@
-import React, {useMemo} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import React, {useEffect, useMemo, useState} from 'react';
 
-import {NextButton} from 'publish/NextButton';
-import {CustomHeader} from 'publish/CustomHeader';
-import {DiscardButton} from 'publish/DiscardButton';
-import {LocalImagesSelector} from 'publish/LocalImagesSelector';
+import {width} from 'utils/width';
+
+import {NextButton} from '../NextButton';
+import {CustomHeader} from '../CustomHeader';
+import {DiscardButton} from '../DiscardButton';
+import {allPhotosAlbum} from '../allPhotosAlbum';
+import {SelectableImage} from '../SelectableImage';
+import {TakePhotoButton} from '../TakePhotoButton';
+import {useShallowData} from '../ShallowDataContext';
+import {PublishImagesPreview} from '../PublishImagesPreview';
+import {SelectImageAlbumButton} from '../SelectImageAlbumButton';
 import {FooterNavigationLayout} from 'navigation/FooterNavigationLayout';
-import {usePublish} from 'publish/PublishContext';
 
-function ValidatedHeader() {
-  const {state} = usePublish();
-  const refresh = state._localRefreshImagesPreview;
+import CameraRoll from '@react-native-community/cameraroll';
 
-  const canContinue = useMemo(() => {
-    const images = Object.keys(state.images || {});
-    return !!images?.length;
-  }, [refresh]);
+const numberOfCollums = 3;
 
-  return (
-    <CustomHeader
-      title="Publicar"
-      left={canContinue && <DiscardButton />}
-      right={canContinue && <NextButton route="Detail" />}
-    />
+function ValidatedHeader({images}) {
+  let canContinue = false;
+  for (let image in images) {
+    canContinue = true;
+    break;
+  }
+
+  return useMemo(
+    () => (
+      <CustomHeader
+        title="Publicar"
+        left={canContinue && <DiscardButton />}
+        right={canContinue && <NextButton route="Detail" />}
+      />
+    ),
+    [canContinue],
   );
 }
 
 export function PublishImagesScreen() {
+  const [album, setAlbum] = useState(allPhotosAlbum);
+  const [foundImages, setFoundImages] = useState([]);
+  const [images, setImages] = useState({});
+  const {data} = useShallowData();
+
+  async function getPhotos() {
+    CameraRoll.getPhotos({
+      groupName: album !== allPhotosAlbum ? album : undefined,
+      first: 100,
+    })
+      .then(res => {
+        setFoundImages(res.edges.map(edge => edge.node.image.uri));
+      })
+      .catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, [album]);
+
   return (
-    <>
-      <FooterNavigationLayout selected="Publish">
-        <ValidatedHeader />
-        <LocalImagesSelector />
-      </FooterNavigationLayout>
-    </>
+    <FooterNavigationLayout selected="Publish">
+      <ValidatedHeader images={images} />
+      <FlatList
+        data={foundImages}
+        numColumns={numberOfCollums}
+        keyExtractor={item => item}
+        ListHeaderComponent={
+          <View style={{backgroundColor: 'white'}}>
+            <PublishImagesPreview />
+            <View style={styles.wrapper}>
+              <SelectImageAlbumButton
+                album={album}
+                setAlbum={setAlbum}
+                style={styles.button}
+              />
+              <TakePhotoButton style={styles.button} />
+            </View>
+          </View>
+        }
+        showsVerticalScrollIndicator={false}
+        getItemLayout={(data, index) => {
+          const size = width / numberOfCollums;
+          return {
+            length: size,
+            offset: (size * index) % numberOfCollums,
+            index,
+          };
+        }}
+        renderItem={({item: uri}) => (
+          <SelectableImage
+            id="images"
+            data={data}
+            key={uri}
+            uri={uri}
+            images={images}
+            setImages={setImages}
+          />
+        )}
+      />
+    </FooterNavigationLayout>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
