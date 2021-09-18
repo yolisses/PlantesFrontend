8f63437f9a876eebda0c71a1a073b7cacd988ccd
Ulisses <m.stunik@gmail.com>
2021-09-18 14:01:22 -0300

    create format to plant

--- src/send/SendingContext.js
+++ src/send/SendingContext.js
@@ -4,6 +4,7 @@ import {useImages} from 'publish/ImagesContext';
 import {useShallowData} from 'publish/ShallowDataContext';
 import React, {createContext, useContext, useEffect, useState} from 'react';
 import {Button} from 'react-native';
+import {formatToPlant} from './formatToPlant';
 
 const SendingContext = createContext();
 
@@ -17,59 +18,31 @@ const CREATE_PLANT = gql`
   }
 `;
 
-// `{
-//   name: "Testando com valor padrão"
-//   images: [
-//     "https://plantei.s3.sa-east-1.amazonaws.com/items/original/9559add5b17f7f18d21c0dad3b8723dd.jpg"
-//   ]
-//   type: "bulb"
-//   swap: true
-//   sell: true
-//   donate: true
-//   price: 10.50
-//   description: "Vamo que vamo"
-//   tags: ["frutífera", "ornamental", "medicinal", "de sombra"]
-//   amount: 12
-// }
-// ) {
-// id
-// name
-// card
-// images
-// }`
-
 export function SendingContextProvider({children}) {
   const [sending, setSending] = useState();
   const {discard: discardImagesSelection} = useImages();
   const {data: shallowData, discard: discardShallowData} = useShallowData();
 
-  const [mutateFunction, {data, loading, error}] = useMutation(CREATE_PLANT);
+  // const [mutateFunction, {data, loading, error}] = useMutation(CREATE_PLANT);
 
   async function send() {
-    console.error('send');
-    const copy = {...shallowData};
+    const sending = formatToPlant(shallowData);
+    setSending(sending);
     discardImagesSelection();
     discardShallowData();
-    setSending(copy);
   }
 
   function onPress() {
-    mutateFunction({
-      variables: {
-        plant: {
-          name: 'vamosquevamos',
-          images: [
-            'https://plantei.s3.sa-east-1.amazonaws.com/items/card/1.webp',
-          ],
-          type: 'bulb',
-        },
-      },
-    });
+    // mutateFunction({
+    //   variables: {inpt
+    //   },
+    // });
+    formatToPlant(sending);
   }
 
-  useEffect(() => {
-    console.error(data, loading, error);
-  }, [data, loading, error]);
+  // useEffect(() => {
+  //   console.error(data, loading, error);
+  // }, [data, loading, error]);
 
   return (
     <SendingContext.Provider value={{send, sending, setSending}}>
