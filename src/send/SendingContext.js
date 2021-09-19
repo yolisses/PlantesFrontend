0a56800ad15515d631e432801cde2f3e106f3ea8
Ulisses <m.stunik@gmail.com>
2021-09-19 15:55:03 -0300

    creact retry system

--- src/send/SendingContext.js
+++ src/send/SendingContext.js
@@ -1,9 +1,29 @@
 import React, {createContext, useContext, useEffect} from 'react';
-import {useMutation} from '@apollo/client';
 import gql from 'graphql-tag';
 import {v4} from 'uuid';
+import {Button} from 'react-native';
+import {api} from 'api/api';
+import {dispatchAllImages} from './dispatchSending';
 
 const SendingContext = createContext();
+
+const images1 = [
+  {localUri: 'file://coisa1.jpg'},
+  {localUri: 'file://coisa2.jpg'},
+  {localUri: 'file://coisa3.jpg'},
+];
+const images2 = [
+  {localUri: 'file://coisa4.jpg'},
+  {localUri: 'file://coisa5.jpg'},
+  {localUri: 'file://coisa6.jpg'},
+];
+const images3 = [
+  {localUri: 'file://coisa7.jpg'},
+  {localUri: 'file://coisa8.jpg'},
+  {localUri: 'file://coisa9.jpg'},
+];
+
+const testSendings = [images1, images2, images3];
 
 const CREATE_PLANT = gql`
   mutation CreatePlant($plant: PlantInput) {
@@ -14,26 +34,50 @@ const CREATE_PLANT = gql`
   }
 `;
 
+const GET_PLANT_IMAGES_LINKS = gql`
+  mutation GetImagesLinks($amount: INT) {
+    getPlantImagesLinks(input: $plant)
+  }
+`;
+
 const sendings = {};
 
 export function SendingContextProvider({children}) {
-  const [mutateFunction, {data, loading, error}] = useMutation(CREATE_PLANT);
+  async function pushSending(plant) {
+    sendings[v4()] = plant;
+    const links = await getPlantImagesLinks();
+    console.error(links);
 
-  async function pushSending(sending) {
-    sendings[v4()] = sending;
-    console.error(sendings);
-    mutateFunction({
-      variables: {plant: sending},
-    });
+    // mutateFunction({
+    //   variables: {plant: sending},
+    // });
+  }
+
+  // useEffect(() => {
+  //   console.error(data, loading, error);
+  // }, [data, loading, error]);
+
+  async function getPlantImagesLinks() {
+    try {
+      const res = await api.post('/graphql', {
+        query: `{
+        getPlantImagesLinks(amount:10)
+      }`,
+      });
+      return res.data.data.getPlantImagesLinks;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   useEffect(() => {
-    console.error(data, loading, error);
-  }, [data, loading, error]);
+    dispatchAllSendings();
+  });
 
   return (
     <SendingContext.Provider value={{pushSending, sendings}}>
       {children}
+      <Button title="teste" onPress={getPlantImagesLinks} />
     </SendingContext.Provider>
   );
 }
