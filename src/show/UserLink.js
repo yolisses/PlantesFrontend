d6928260cfa3e5e3c2d6e733af2e7425fd4dc407
Ulisses <m.stunik@gmail.com>
2021-09-23 11:50:27 -0300

    fetch user link from api

--- src/show/UserLink.js
+++ src/show/UserLink.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import {api} from 'api/api';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 
 export function UserLink(userId) {
+  const [user, setUser] = useState();
+
+  async function getUser() {
+    const res = await api.get('/user/614c85e97244c7e73c35ca5c');
+    setUser(res.data);
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <FastImage style={styles.image} />
-      <Text style={styles.name}>Ulisses</Text>
+      <FastImage style={styles.image} source={{uri: user?.image}} />
+      <Text style={styles.name}>{user?.name}</Text>
     </View>
   );
 }
@@ -18,7 +34,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   image: {
-    width: 48,
+    width: 40,
     aspectRatio: 1,
     marginRight: 10,
     borderRadius: 100,
