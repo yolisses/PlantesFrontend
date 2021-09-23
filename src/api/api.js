c8e1796c82b17230fdbeaad3046ee230bd734047
Ulisses <m.stunik@gmail.com>
2021-09-23 11:13:12 -0300

    add cache adapter

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import {setupCache} from 'axios-cache-adapter';
 
 import {SERVER_URL} from '@env';
 
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000, // 15 minutes
+});
+
 export const api = axios.create({
+  adapter: cache.adapter,
   baseURL: SERVER_URL,
 });
