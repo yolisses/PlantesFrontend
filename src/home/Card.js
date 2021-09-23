bdc3f6db73bdd4cd0094e1201ff14df0c355d833
Ulisses <m.stunik@gmail.com>
2021-09-23 10:56:01 -0300

    remove name indicator

--- src/home/Card.js
+++ src/home/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {SquareImage} from 'common/SquareImage';
-import {Text, TouchableOpacity} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 export function Card({item, fraction}) {
@@ -16,7 +16,6 @@ export function Card({item, fraction}) {
   return (
     <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
       <SquareImage uri={item?.card} fraction={fraction || 2} />
-      <Text>{item?.name}</Text>
     </TouchableOpacity>
   );
 }
