3475b219d5b1008e0a9eaea383113d1e834338d5
Ulisses <m.stunik@gmail.com>
2021-09-29 12:22:40 -0300

    add image preview on publish

--- src/publish/PublishImagesPreview.js
+++ src/publish/PublishImagesPreview.js
@@ -1,13 +1,15 @@
+import {useObserver} from 'mobx-react-lite';
 import React from 'react';
 import {ImagesSwiper} from 'show/ImagesSwiper';
 import {ChooseImagesPlaceholder} from './ChooseImagesPlaceholder';
 import {selectedImages} from './selectedImages';
 
 export function PublishImagesPreview() {
-  const images = Object.keys(selectedImages || {});
-
-  if (!images.length) {
-    return <ChooseImagesPlaceholder />;
-  }
-  return <ImagesSwiper images={images} />;
+  return useObserver(() => {
+    const images = Object.keys(selectedImages || {});
+    if (!images.length) {
+      return <ChooseImagesPlaceholder />;
+    }
+    return <ImagesSwiper images={images} />;
+  });
 }
