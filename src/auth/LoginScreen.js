6cb3b582536ed2c2f8ca2eec136439688c7c211f
Ulisses <m.stunik@gmail.com>
2021-09-30 15:27:32 -0300

    add simple user and token persist

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,8 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {GoogleSignInButton} from 'auth/GoogleSignInButton';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {auth} from './auth';
 
 export function LoginScreen() {
+  useEffect(() => {
+    AsyncStorage.getItem('userInfo').then(res => {
+      try {
+        const {token, user} = JSON.parse(res);
+        auth.user = user;
+        auth.userId = user._id;
+        auth.token = token;
+      } catch (err) {
+        console.error(err);
+      }
+      console.error(JSON.parse(res));
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.logo}>Plantei</Text>
--- src/auth/authenticate.js
+++ src/auth/authenticate.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {api} from 'api';
 import OneSignal from 'react-native-onesignal';
 import {auth} from './auth';
@@ -5,22 +6,22 @@ import {auth} from './auth';
 export async function authenticate(idToken) {
   try {
     const res = await api.post('/google-sign-in', {idToken});
-    const {token, user, email, emailAuthToken, id, idAuthToken} = res.data;
+    const {token, user, email, emailAuthToken, idAuthToken} = res.data;
     auth.user = user;
-    auth.userId = id;
+    auth.userId = user._id;
     auth.token = token;
-
-    console.error(auth);
 
     OneSignal.setEmail(email, emailAuthToken, err => {
       console.error(err);
     });
 
-    OneSignal.setExternalUserId(id, idAuthToken, err => {
+    OneSignal.setExternalUserId(user._id, idAuthToken, err => {
       console.error(err);
     });
 
     api.defaults.headers.common.auth = `Bearer ${token}`;
+
+    AsyncStorage.setItem('userInfo', JSON.stringify({token, user, email}));
   } catch (err) {
     console.error(err);
   }
