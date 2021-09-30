4066122b920bfb0416e09ad25ea28c4351209b8f
Ulisses <m.stunik@gmail.com>
2021-09-30 12:02:34 -0300

    create simple update profile

--- src/profile/EditProfileScreen.js
+++ src/profile/EditProfileScreen.js
@@ -8,40 +8,55 @@ import {TextInput} from 'form/TextInput';
 import {BackButton} from 'publish/BackButton';
 import {NextButton} from 'publish/NextButton';
 import {CustomHeader} from 'publish/CustomHeader';
+import {api} from 'api';
+import {useObserver} from 'mobx-react-lite';
+import {editProfileData} from './editProfileData';
+import {useNavigation} from '@react-navigation/core';
 
 export function EditProfileScreen() {
-  return (
-    <>
-      <CustomHeader
-        left={<BackButton />}
-        title="Editar perfil"
-        right={<NextButton text="Salvar" hideIcon />}
-      />
-      <ScrollView style={styles.container}>
-        <FastImage
-          disableNavigate
-          style={styles.image}
-          source={{uri: auth?.user?.image}}
+  const {goBack} = useNavigation();
+  return useObserver(() => {
+    function updateProfile() {
+      console.error(editProfileData);
+      api
+        .put('/update-profile', editProfileData)
+        .then(res => {
+          console.error(res.data);
+          auth.user = res.data;
+          goBack();
+        })
+        .catch(err => console.error(err.response));
+    }
+
+    return (
+      <>
+        <CustomHeader
+          left={<BackButton />}
+          title="Editar perfil"
+          right={
+            <NextButton text="Salvar" hideIcon customOnPress={updateProfile} />
+          }
         />
-        <TouchableOpacity activeOpacity={0.7}>
-          <Text style={styles.link}>Alterar foto do perfil</Text>
-        </TouchableOpacity>
-        <TextInput
-          data={{}}
-          label="Nome"
-          id="userName"
-          customGetInitialValue={() => auth.user.name}
-        />
-        <TextInput
-          multiline
-          data={{}}
-          id="description"
-          label="Descrição"
-          customGetInitialValue={() => auth.user.description}
-        />
-      </ScrollView>
-    </>
-  );
+        <ScrollView style={styles.container}>
+          <FastImage
+            disableNavigate
+            style={styles.image}
+            source={{uri: auth?.user?.image}}
+          />
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text style={styles.link}>Alterar foto do perfil</Text>
+          </TouchableOpacity>
+          <TextInput label="Nome" id="name" data={editProfileData} />
+          <TextInput
+            multiline
+            id="description"
+            label="Descrição"
+            data={editProfileData}
+          />
+        </ScrollView>
+      </>
+    );
+  });
 }
 
 const styles = StyleSheet.create({
