d79c583eee91f7c69b065145f859102069cf174b
Ulisses <m.stunik@gmail.com>
2021-09-29 21:32:54 -0300

    fix home loading

--- src/home/Card.js
+++ src/home/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import {SquareImage} from 'common/SquareImage';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {width} from 'utils/width';
 
 export function Card({item, fraction, postComponent}) {
   const {navigate} = useNavigation();
@@ -15,13 +16,33 @@ export function Card({item, fraction, postComponent}) {
 
   return (
     <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
-      <SquareImage uri={item?.card} fraction={fraction || 2} />
+      <SquareImage
+        uri={item?.card}
+        fraction={fraction || 2}
+        style={styles.card}
+      />
       {postComponent}
-      <View style={{flexDirection: 'row'}}>
-        <Text>{item.donate ? 'Doação ' : ''}</Text>
-        <Text>{item.swap ? 'Troca ' : ''}</Text>
-        <Text>{item.price ? 'R$' + item.price + ' ' : ''}</Text>
-      </View>
+      {/* <View style={styles.textContainer}>
+        <Text style={styles.text}>{item.donate ? 'Doação ' : ''}</Text>
+        <Text style={styles.text}>{item.swap ? 'Troca ' : ''}</Text>
+        <Text style={styles.text}>{item.price ? 'R$' + item.price : ''}</Text>
+      </View> */}
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+  },
+  textContainer: {
+    width: width / 2,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingBottom: 10,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
