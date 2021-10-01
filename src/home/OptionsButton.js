7e19ddff86b8e1fe4fa2ecc9387b13abfd1c7577
Ulisses <m.stunik@gmail.com>
2021-10-01 12:15:25 -0300

    add padding

--- src/home/OptionsButton.js
+++ src/home/OptionsButton.js
@@ -14,6 +14,7 @@ export function OptionsButton({text, ...rest}) {
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 5,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
