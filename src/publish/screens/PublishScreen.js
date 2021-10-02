42ad7d6f4da3d28b30599a3c452f95ead0ab81c4
Ulisses <m.stunik@gmail.com>
2021-10-02 07:40:40 -0300

    add crop and picker

--- src/publish/screens/PublishScreen.js
+++ src/publish/screens/PublishScreen.js
@@ -1,36 +1,21 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import {PublishImagesScreen} from './PublishImagesScreen';
-import {PublishDetailScreen} from './PublishDetailScreen';
-import {PublishPriceScreen} from './PublishPriceScreen';
-import {CameraScreen} from 'camera/CameraScreen';
-
-const Publish = createNativeStackNavigator();
+import {Button, View} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export function PublishScreen() {
+  function onPress() {
+    ImagePicker.openPicker({
+      width: 200,
+      height: 200,
+      cropping: true,
+    }).then(image => {
+      console.error(image);
+    });
+  }
+
   return (
-    <Publish.Navigator>
-      <Publish.Screen
-        name="Images"
-        component={PublishImagesScreen}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Publish.Screen
-        name="Detail"
-        component={PublishDetailScreen}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Publish.Screen
-        name="Price"
-        component={PublishPriceScreen}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-      <Publish.Screen
-        name="Camera"
-        component={CameraScreen}
-        options={{headerShown: false, animationEnabled: false}}
-      />
-    </Publish.Navigator>
+    <View>
+      <Button title="open" onPress={onPress} />
+    </View>
   );
 }
