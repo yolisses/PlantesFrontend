64688034162dfc9e3f9ba002aaf947fad14a3230
Ulisses <m.stunik@gmail.com>
2021-10-02 10:56:05 -0300

    add images on select images field

--- src/publish/SelectImagesField.js
+++ src/publish/SelectImagesField.js
@@ -1,10 +1,14 @@
 import {faSeedling} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {useNavigation} from '@react-navigation/core';
+import {SquareImage} from 'common/SquareImage';
 import {Fieldset} from 'form/Fieldset';
+import {useObserver} from 'mobx-react-lite';
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native';
 import {StyleSheet, Text, View} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import {selectedImages} from './selectedImages';
 
 export function SelectImagesField() {
   const {navigate} = useNavigation();
@@ -13,41 +17,73 @@ export function SelectImagesField() {
     navigate('Images');
   }
 
-  return (
+  return useObserver(() => (
     //   <Text style={styles.text}>Selecionar imagens</Text>
     <View style={styles.container}>
       <Fieldset label="Fotos" styleLabel={styles.label}>
-        <TouchableOpacity
-          style={styles.inner}
-          activeOpacity={0.7}
-          onPress={onPress}>
-          <View style={styles.imagePlaceholder}>
-            <FontAwesomeIcon icon={faSeedling} size={60} color="#aaa" />
-          </View>
-          <Text style={styles.text}>Selecionar fotos</Text>
-        </TouchableOpacity>
+        <ScrollView
+          horizontal
+          style={{flex: 1}}
+          contentContainerStyle={[
+            styles.inner,
+            !Object.keys(selectedImages).length && {flex: 1},
+          ]}
+          showsHorizontalScrollIndicator={false}>
+          {/* {!Object.keys(selectedImages).length && (
+            <View style={styles.imagePlaceholder}>
+              <FontAwesomeIcon icon={faSeedling} size={60} color="#aaa" />
+            </View>
+          )} */}
+          {Object.keys(selectedImages).map(uri => (
+            <FastImage
+              source={{uri}}
+              fraction={4}
+              style={styles.image}
+              key={uri}
+            />
+          ))}
+          <TouchableOpacity
+            style={styles.select}
+            activeOpacity={0.7}
+            onPress={onPress}>
+            <Text style={styles.text}>Selecionar</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </Fieldset>
     </View>
-  );
+  ));
 }
+
+const offset = 2;
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 18,
+    flexWrap: 'wrap',
+  },
+  select: {
+    height: 160,
+    backgroundColor: '#eee',
+    padding: 10,
+    flex: 1,
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: offset,
+    justifyContent: 'center',
   },
   inner: {
     flexDirection: 'row',
-    flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
+    padding: offset,
   },
   container: {
     alignItems: 'stretch',
-    marginTop: 20,
+    // marginTop: 20,
   },
   label: {
-    position: 'absolute',
-    zIndex: 10,
-    transform: [{translateY: -20}],
+    // position: 'absolute',
+    // zIndex: 10,
+    // transform: [{translateY: -20}],
     backgroundColor: 'white',
   },
   imagePlaceholder: {
@@ -59,5 +95,11 @@ const styles = StyleSheet.create({
     margin: 10,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    height: 160,
+    borderRadius: 10,
+    margin: offset,
+    aspectRatio: 1,
   },
 });
