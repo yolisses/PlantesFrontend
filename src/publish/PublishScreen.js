3e79ac99c6ace3ae0b5b4fb1bc042970c83b5556
Ulisses <m.stunik@gmail.com>
2021-10-03 11:46:36 -0300

    remove publishdata from publishscreen

--- src/publish/PublishScreen.js
+++ src/publish/PublishScreen.js
@@ -1,86 +1,35 @@
 import {IntInput} from 'form/IntInput';
-import {PriceInput} from 'form/PriceInput';
 import {TagsSelector} from 'form/TagsSelector';
 import {TextInput} from 'form/TextInput';
 import {useObserver} from 'mobx-react-lite';
 import {FooterNavigationLayout} from 'navigation/FooterNavigationLayout';
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {CustomHeader} from './CustomHeader';
 import {availabilities, availabilitiesLabels} from './data/availiabilities';
 import {tags} from './data/tags';
 import {FinishButton} from './FinishButton';
-import {publishData} from './publishData';
 import {SelectImagesField} from './SelectImagesField';
 
 export function PublishScreen() {
-  const errorsObj = {};
-  const [forceValidate, setForceValidate] = useState();
-
-  function checkItsValid() {
-    setForceValidate(Math.random());
-    for (let key in errorsObj) {
-      if (errorsObj[key]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  const validateText = text => {
-    if (!text) {
-      return 'por favor informe o nome';
-    }
-    if (text.trim().length < 3) {
-      return 'precisa ter pelo menos tres letras';
-    }
-  };
-
   return useObserver(() => (
     <FooterNavigationLayout selected="Publish">
-      <CustomHeader
-        // left={<DiscardButton />}
-        title="Publicar"
-        right={<FinishButton checkItsValid={checkItsValid} />}
-      />
+      <CustomHeader title="Publicar" right={<FinishButton />} />
       <ScrollView style={styles.container}>
         <SelectImagesField />
-        <TextInput
-          id="name"
-          forceValidate={forceValidate}
-          errorsObj={errorsObj}
-          label="Nome"
-          maxLength={32}
-          validate={validateText}
-          data={publishData}
-        />
+        <TextInput id="name" label="Nome" maxLength={32} />
         <TagsSelector
           showIcon={false}
-          data={publishData}
           id="availabilities"
           label="Disponível para"
           options={availabilities}
           buttonStyle={styles.button}
           labels={availabilitiesLabels}
         />
-        {publishData.availabilities?.sell && (
-          <PriceInput id="price" data={publishData} label="Preço" />
-        )}
-        <TagsSelector
-          id="tags"
-          data={publishData}
-          label="Marcar como"
-          options={tags}
-        />
-        <TextInput
-          id="description"
-          data={publishData}
-          label="Descrição"
-          optional
-          multiline
-        />
-        <IntInput id="amount" data={publishData} label="Quantidade" optional />
+        <TagsSelector id="tags" label="Marcar como" options={tags} />
+        <TextInput id="description" label="Descrição" optional multiline />
+        <IntInput id="amount" label="Quantidade" optional />
       </ScrollView>
     </FooterNavigationLayout>
   ));
