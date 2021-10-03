a6ece97aace12908b22b5078922a12e367f1d6ee
Ulisses <m.stunik@gmail.com>
2021-10-03 13:10:25 -0300

    create working example of togle button

--- src/dev/Dev.js
+++ src/dev/Dev.js
@@ -1,64 +1,53 @@
 import React, {useRef} from 'react';
+import {Text, View, Button, TouchableOpacity} from 'react-native';
 import {Formik} from 'formik';
-import {Button, StyleSheet, ScrollView} from 'react-native';
-import * as Yup from 'yup';
-import {TextInput} from 'form/TextInput';
+
+const Checkbox = ({children, value, onValueChange}) => {
+  function onPress(e) {
+    onValueChange(!value);
+  }
+
+  return (
+    <View>
+      <View>
+        <TouchableOpacity onPress={onPress}>
+          {/* <CheckBox
+          type={'checkbox'}
+          value={value}
+          onValueChange={handleChange}
+          checked={value}
+        /> */}
+          {value ? <Text>sim</Text> : <Text>não</Text>}
+          <Text>{children}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Checkbox;
 
 export function Dev() {
-  const user = useRef(null);
-  const password = useRef(null);
-
-  const FormSchema = Yup.object().shape({
-    user: Yup.string().required('Campo obrigatório'),
-    password: Yup.string()
-      .required('Campo obrigatório')
-      .min(8, 'Digite pelo menos 8 caracteres'),
-  });
-
+  const valor = useRef();
   return (
     <Formik
       initialValues={{
-        user: '',
-        password: '',
+        valor: true,
       }}
-      onSubmit={values => {
+      onSubmit={(values, {resetForm}) => {
         console.error(values);
-      }}
-      validationSchema={FormSchema}>
-      {({
-        values,
-        handleChange,
-        handleSubmit,
-        errors,
-        touched,
-        setFieldTouched,
-      }) => (
-        <ScrollView style={styles.container}>
-          <TextInput
-            ref={user}
-            label={'Usuário'}
-            value={values.user}
-            onChangeText={handleChange('user')}
-            error={touched.user && errors.user}
-            onBlur={() => setFieldTouched('user', true)}
-          />
-          <TextInput
-            ref={password}
-            label={'Senha'}
-            value={values.password}
-            onChangeText={handleChange('password')}
-            error={touched.password && errors.password}
-            onBlur={() => setFieldTouched('password', true)}
-          />
-          <Button title="Entrar" onPress={handleSubmit} />
-        </ScrollView>
+      }}>
+      {({handleChange, handleSubmit, values, setFieldValue}) => (
+        <View>
+          <Checkbox
+            ref={valor}
+            value={values?.valor}
+            onValueChange={value => setFieldValue('valor', value)}>
+            Financially Responsible
+          </Checkbox>
+          <Button onPress={handleSubmit} title="Submit" />
+        </View>
       )}
     </Formik>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-  },
-});
