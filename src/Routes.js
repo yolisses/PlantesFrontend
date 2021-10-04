fe49e5441896a72fd81b79e1603c774d15afa8f6
Ulisses <m.stunik@gmail.com>
2021-10-04 16:11:24 -0300

    clean

--- src/Routes.js
+++ src/Routes.js
@@ -10,16 +10,12 @@ import {useObserver} from 'mobx-react-lite';
 import {AlertDisplay} from 'alert/AlertDisplay';
 import {ModalDisplay} from 'modal/ModalDisplay';
 
-import {ChatScreen} from 'chat/ChatScreen';
 import {UserScreen} from 'user/UserScreen';
 import {HomeScreen} from 'home/HomeScreen';
 import {LoginScreen} from 'auth/LoginScreen';
 import {ConfigScreen} from 'config/ConfigScreen';
 import {ShowItemScreen} from 'show/ShowItemScreen';
 import {PublishScreen} from 'publish/PublishScreen';
-import {ChatsListScreen} from 'chat/ChatsListScreen';
-import {CommunityScreen} from 'post/CommunityScreen';
-import {CommentsScreen} from 'comment/CommentsScreen';
 import {EditProfileScreen} from 'profile/EditProfileScreen';
 import {SelectImagesScreen} from 'publish/SelectImagesScreen';
 
@@ -29,11 +25,11 @@ export function Routes() {
   return useObserver(() => (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen
+        <Stack.Screen
           name="dev"
           component={PublishScreen}
           options={{headerShown: false}}
-        /> */}
+        />
         {/* <Stack.Screen name="dev" component={Dev} /> */}
         {!auth.token ? (
           <Stack.Screen
@@ -70,34 +66,14 @@ export function Routes() {
               options={{headerShown: false}}
             />
             <Stack.Screen
-              name="Chat"
-              component={ChatScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="ChatsList"
-              component={ChatsListScreen}
-              options={{title: 'Conversas', headerLeft: () => <></>}}
-            />
-            <Stack.Screen
               name="ShowItem"
               component={ShowItemScreen}
               options={{headerShown: false}}
             />
             <Stack.Screen
-              name="Comments"
-              component={CommentsScreen}
-              options={{title: 'Comentários'}}
-            />
-            <Stack.Screen
               name="EditProfile"
               component={EditProfileScreen}
               options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="Community"
-              component={CommunityScreen}
-              options={{title: 'Comunidade'}}
             />
           </>
         )}
