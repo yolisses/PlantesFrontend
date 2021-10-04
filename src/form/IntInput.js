c2853ad64a6a4350f5ae5c7d250f9b9bab7d504f
Ulisses <m.stunik@gmail.com>
2021-10-04 12:27:56 -0300

    clear int input

--- src/form/IntInput.js
+++ src/form/IntInput.js
@@ -1,30 +1,14 @@
 import React from 'react';
 
 import {TextInput} from 'form/TextInput';
-import {allowRegexInt} from 'form/allowRegex/allowRegexInt';
 
-export function IntInput({id, data, ...rest}) {
-  function onChangeText(text, setValue) {
-    const numberText = text.match(allowRegexInt);
-    if (numberText) {
-      const number = Number(numberText[0]);
-      data[id] = number;
-      setValue(number);
-    } else {
-      data[id] = null;
-      setValue(null);
-    }
-  }
-
+export function IntInput({...rest}) {
   return (
     <TextInput
       {...rest}
-      id={id}
-      data={data}
       autoCorrect={false}
       autoCompleteType={'off'}
       keyboardType="number-pad"
-      customOnChangeText={onChangeText}
     />
   );
 }
