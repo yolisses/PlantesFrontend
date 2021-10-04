7936a403b3d40cf012b4695e7a71686799f11edc
Ulisses <m.stunik@gmail.com>
2021-10-04 12:26:03 -0300

    add sell with price validation

--- src/form/PriceInput.js
+++ src/form/PriceInput.js
@@ -3,51 +3,16 @@ import React from 'react';
 import {TextInput} from 'form/TextInput';
 
 import {MoneySign} from './MoneySign';
-import {allowRegexMoney} from './allowRegex/allowRegexMoney';
-import {numberToMoney} from 'utils/numberToMoney';
 
-export function PriceInput({id, data, onChangeValue, ...rest}) {
-  function onBlur(e, value, setValue) {
-    const number = data[id];
-    if (number === null) {
-      setValue(null);
-    } else {
-      setValue(numberToMoney(number));
-    }
-  }
-
-  function onChangeText(text, setValue) {
-    const numberText = text.match(allowRegexMoney);
-
-    const newNumber = numberText
-      ? Number(numberText[0].replace(',', '.'))
-      : null;
-    data[id] = newNumber;
-    if (onChangeValue) {
-      onChangeValue(newNumber);
-    }
-
-    const newText = numberText ? numberText[0] : null;
-    setValue(newText);
-  }
-
-  function getInitialValue() {
-    if (data[id]) {
-      return numberToMoney(data[id]);
-    }
-    return null;
-  }
-
+export function PriceInput({...rest}) {
+  console.error();
   return (
     <TextInput
       {...rest}
       autoCorrect={false}
-      customOnBlur={onBlur}
       autoCompleteType={'off'}
       leftChild={<MoneySign />}
       keyboardType="decimal-pad"
-      customOnChangeText={onChangeText}
-      customGetInitialValue={getInitialValue}
     />
   );
 }
