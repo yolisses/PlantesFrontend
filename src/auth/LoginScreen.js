7013b8c5a0e726427b174646207c4b017f883b87
Ulisses <m.stunik@gmail.com>
2021-10-05 20:40:31 -0300

    add logo to login screen

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,11 +1,18 @@
 import {GoogleSignInButton} from 'auth/GoogleSignInButton';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 export function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>Plantei</Text>
+      <View style={styles.wrapper}>
+        <FastImage
+          source={require('../../icon/icon.png')}
+          style={styles.image}
+        />
+        <Text style={styles.logo}>Plantes</Text>
+      </View>
       <Text style={styles.subtitle}>
         Se cadastre para trocar plantas perto de você
       </Text>
@@ -22,12 +29,19 @@ const styles = StyleSheet.create({
   },
   logo: {
     fontSize: 28,
-    marginBottom: 10,
+    marginBottom: 5,
   },
   subtitle: {
     fontSize: 18,
-    marginBottom: 30,
+    marginBottom: 20,
     color: 'gray',
-    padding: 20,
+    paddingHorizontal: 20,
+  },
+  wrapper: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 60,
+    aspectRatio: 1,
   },
 });
