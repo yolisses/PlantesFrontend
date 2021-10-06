b6a4a5a682d8ea3ac7217caf783b2596974e0978
Ulisses <m.stunik@gmail.com>
2021-10-06 19:26:38 -0300

    remove edit profile data

--- src/profile/EditProfileScreen.js
+++ src/profile/EditProfileScreen.js
@@ -1,4 +1,4 @@
-import React, {createRef, useEffect, useState} from 'react';
+import React, {createRef, useState} from 'react';
 import FastImage from 'react-native-fast-image';
 import {ScrollView, StyleSheet} from 'react-native';
 
@@ -8,11 +8,8 @@ import {BackButton} from 'publish/BackButton';
 import {NextButton} from 'publish/NextButton';
 import {CustomHeader} from 'publish/CustomHeader';
 import {api} from 'api';
-import {useObserver} from 'mobx-react-lite';
-import {editProfileData} from './editProfileData';
 import {useNavigation} from '@react-navigation/core';
 import {useUserById} from 'common/UsersByIdContext';
-import {observe} from 'mobx';
 import {CepInput} from 'form/CepInput';
 import {Keyboard} from 'react-native';
 
@@ -21,101 +18,77 @@ export function EditProfileScreen() {
   const {goBack} = useNavigation();
   const {setCurrentUser} = useUserById();
 
-  if (!editProfileData.errors) {
-    editProfileData.errors = {};
-  }
-
   const ref = createRef();
 
-  return useObserver(() => {
-    async function updateProfile() {
-      Keyboard.dismiss();
-      if (editProfileData.errors.name || editProfileData.errors.cep) {
-        return;
-      }
-      if (editProfileData.cep && editProfileData.cepValid) {
-        console.error(ref);
-      }
-      ref?.current?.focus();
-      setSaving(true);
-      api
-        .put('/update-profile', editProfileData)
-        .then(res => {
-          auth.user = res.data;
-          setCurrentUser(res.data);
-          setSaving(false);
-          goBack();
-        })
-        .catch(err => {
-          setSaving(false);
-          console.error(err.response);
-        });
+  async function updateProfile() {
+    Keyboard.dismiss();
+    ref?.current?.focus();
+    setSaving(true);
+    api
+      .put('/update-profile', {})
+      .then(res => {
+        auth.user = res.data;
+        setCurrentUser(res.data);
+        setSaving(false);
+        goBack();
+      })
+      .catch(err => {
+        setSaving(false);
+        console.error(err.response);
+      });
+  }
+
+  function validateName({setError, id, data, text}) {
+    if (!text || text.length < 3) {
+      data.errors[id] = true;
+      setError('O nome precisa de no mínimo 3 letras');
+    } else {
+      setError();
+      data.errors[id] = false;
     }
+  }
 
-    useEffect(() => {
-      observe(editProfileData, () => {});
-    }, []);
-
-    function validateName({setError, id, data, text}) {
-      if (!text || text.length < 3) {
-        data.errors[id] = true;
-        setError('O nome precisa de no mínimo 3 letras');
-      } else {
-        setError();
-        data.errors[id] = false;
-      }
-    }
-
-    return (
-      <>
-        <CustomHeader
-          left={<BackButton />}
-          title="Editar perfil"
-          right={
-            <NextButton
-              ref={ref}
-              hideIcon
-              onPress={updateProfile}
-              text={saving ? 'Salvando...' : 'Salvar'}
-            />
-          }
-        />
-        <ScrollView style={styles.container}>
-          <FastImage
-            disableNavigate
-            style={styles.image}
-            source={{uri: auth?.user?.image}}
+  return (
+    <>
+      <CustomHeader
+        left={<BackButton />}
+        title="Editar perfil"
+        right={
+          <NextButton
+            ref={ref}
+            hideIcon
+            onPress={updateProfile}
+            text={saving ? 'Salvando...' : 'Salvar'}
           />
-          {/* <TouchableOpacity activeOpacity={0.7}>
+        }
+      />
+      <ScrollView style={styles.container}>
+        <FastImage
+          disableNavigate
+          style={styles.image}
+          source={{uri: auth?.user?.image}}
+        />
+        {/* <TouchableOpacity activeOpacity={0.7}>
             <Text style={styles.link}>Alterar foto do perfil</Text>
           </TouchableOpacity> */}
-          <TextInput
-            id="name"
-            label="Nome"
-            maxLength={32}
-            // blurValidate={validateName}
-            textValidate={validateName}
-            data={editProfileData}
-            customGetInitialValue={() => auth?.user?.name}
-          />
-          <CepInput
-            id="cep"
-            data={editProfileData}
-            customGetInitialValue={() => auth?.user?.cep}
-          />
-          <TextInput
-            optional
-            multiline
-            id="description"
-            maxLength={400}
-            label="Descrição"
-            data={editProfileData}
-            customGetInitialValue={() => auth?.user?.description}
-          />
-        </ScrollView>
-      </>
-    );
-  });
+        <TextInput
+          id="name"
+          label="Nome"
+          maxLength={32}
+          // blurValidate={validateName}
+          textValidate={validateName}
+          customGetInitialValue={() => auth?.user?.name}
+        />
+        <TextInput
+          optional
+          multiline
+          maxLength={400}
+          label="Descrição"
+          customGetInitialValue={() => auth?.user?.description}
+        />
+      </ScrollView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
