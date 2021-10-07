7248fe631883920e2bc878b9d4c44f50385780a1
Ulisses <m.stunik@gmail.com>
2021-10-07 10:18:43 -0300

    clean

--- src/messages/InstagramButton.js
+++ src/messages/InstagramButton.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {StyleSheet, Linking} from 'react-native';
 import {SendMessageButton} from './SendMessageButton';
-import {faInstagram, faWhatsapp} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 export function InstagramButton() {
   async function onPress() {
-    const supported = await Linking.canOpenURL('instagram://app');
-    // if (supported) {
-    return Linking.openURL('instagram://user?username=zacefron');
-    // } else {
-    //   return Linking.openURL('https://api.instagram.com');
-    // }
+    const supported = await Linking.canOpenURL(
+      'instagram://user?username=zacefron',
+    );
+    if (supported) {
+      return Linking.openURL('instagram://user?username=zacefron');
+    } else {
+      return Linking.openURL('https://api.instagram.com/zacefron');
+    }
   }
 
   return (
     <SendMessageButton
       icon={faInstagram}
       onPress={onPress}
-      activeOpacity={0.9}
-      iconColor={'#a630bb'}
+      activeOpacity={0.97}
+      iconColor={'#9620dd'}
       textStyle={styles.text}
       buttonStyle={styles.button}
     />
@@ -26,12 +28,8 @@ export function InstagramButton() {
 }
 
 const styles = StyleSheet.create({
-  button: {
-    // backgroundColor: 'green',
-  },
-  text: {
-    // color: '#a630bb',
-  },
+  button: {},
+  text: {},
   icon: {
     marginLeft: 10,
   },
