11ced04db5e3fb2288d6a39ecd6f101739044a17
Ulisses <m.stunik@gmail.com>
2021-10-09 10:20:44 -0300

    invalidate plant on remove

--- src/show/RemoveItemAlert.js
+++ src/show/RemoveItemAlert.js
@@ -1,13 +1,22 @@
+import {useNavigation} from '@react-navigation/core';
 import {Alert} from 'alert/Alert';
 import {AlertButton} from 'alert/AlertButton';
 import {api} from 'api/api';
+import {auth} from 'auth/auth';
 import React from 'react';
+import {useQueryClient} from 'react-query';
 
 export function RemoveItemAlert({item}) {
+  const queryClient = useQueryClient();
+
+  const {goBack} = useNavigation();
+
   async function onRemovePress() {
     try {
-      const res = await api.delete('plant/' + item?._id);
-      console.error(res.data);
+      await api.delete('plant/' + item?._id);
+      queryClient.invalidateQueries('plants');
+      queryClient.invalidateQueries(['user', 'plants', auth.userId]);
+      goBack();
     } catch (err) {
       console.error(err);
     }
