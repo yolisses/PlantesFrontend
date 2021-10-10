6853449fb85ed1b3590940ca7dcba5737cbf1dbe
Ulisses <m.stunik@gmail.com>
2021-10-10 14:47:33 -0300

    fix old results shown while searching

--- src/home/HomeScreen.js
+++ src/home/HomeScreen.js
@@ -51,7 +51,7 @@ export function HomeScreen() {
 
   useEffect(() => {
     observe(searchOptions, () => {
-      queryClient.invalidateQueries('plants');
+      queryClient.refetchQueries('plants');
     });
   }, []);
 
