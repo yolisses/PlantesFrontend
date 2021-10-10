54a503f1937820e882812dc6b1a8c8f6400248d7
Ulisses <m.stunik@gmail.com>
2021-10-10 16:33:00 -0300

    first try

--- src/dev/Dev.js
+++ src/dev/Dev.js
@@ -1,30 +1,15 @@
 import {SquareImage} from 'common/SquareImage';
 import React from 'react';
 import {Button, View} from 'react-native';
-import ImageResizer from 'react-native-image-resizer';
+import ImageCropPicker from 'react-native-image-crop-picker';
 
 export function Dev() {
   async function onPress() {
     try {
-      const maxWidth = 400;
-      const maxHeight = 400;
-      const outputPath = 'file:///storage/emulated/0/Pictures/Plantei/';
-      const quality = 100;
-      const rotation = 0;
-      const compressFormat = 'WEBP';
-      const path =
-        'storage/emulated/0/Pictures/Plantei/aa93d3b1-1a08-4350-a236-89002445cf5c.jpg';
-
-      const res = await ImageResizer.createResizedImage(
-        path,
-        maxWidth,
-        maxHeight,
-        compressFormat,
-        quality,
-        rotation,
-        undefined,
-        {onlyScaleDown: true},
-      );
+      const res = await ImageCropPicker.openCropper({
+        path: 'file:///storage/emulated/0/Pictures/Plantei/aa93d3b1-1a08-4350-a236-89002445cf5c.jpg',
+        multiple: true,
+      });
       console.error(res);
     } catch (err) {
       console.error(err);
