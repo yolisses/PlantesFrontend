7c9ccc1ba499344574f26e0a0b7046d7e059d97b
Ulisses <m.stunik@gmail.com>
2021-10-10 18:19:17 -0300

    improve image selector

--- src/publish/SelectImagesScreen.js
+++ src/publish/SelectImagesScreen.js
@@ -1,17 +1,19 @@
 import {useNavigation} from '@react-navigation/core';
 import React from 'react';
 
-import {Controller} from 'react-hook-form';
+import {Controller, useForm} from 'react-hook-form';
 import {ImagesPicker} from './ImagesPicker';
 
 export function SelectImagesScreen({route}) {
-  const {control, onChange: routeOnChange} = route.params;
+  const {onChange: routeOnChange, value} = route.params;
   const {goBack} = useNavigation();
+  const {control} = useForm();
+
   return (
     <Controller
       name="images"
       control={control}
-      defaultValue={{}}
+      defaultValue={{...value}}
       render={({field: {onChange, value}}) => (
         <ImagesPicker
           value={value}
