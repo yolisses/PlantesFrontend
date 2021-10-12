0e45a605193a514c6432675cce76b1a9d6d03540
Ulisses <m.stunik@gmail.com>
2021-10-12 11:52:09 -0300

    fix reset form crash

--- src/send/ship.ts
+++ src/send/ship.ts
@@ -1,35 +1,43 @@
+import {send} from './sendings';
 import {sendImage} from './sendImage';
 import {waitSomeTime} from './waitSomeTime';
+import {compressImage} from './compressImage';
 import {confirmSending} from './confirmSending';
 import {formatFormToItemInfo} from './formatFormToItemInfo';
-import {getNewImageByLocalUri} from './getNewImageByLocalUri';
 import {sendItemCreationRequest} from './sendItemCreationRequest';
 import {associateLocalAndRemoteImages} from './associateLocalAndRemoteImages';
-import {send} from './sendings';
 
 export async function ship(itemFormData: ItemFormData, callback: () => any) {
   const id = Math.random();
 
   const shipment: Shipment = {
     id,
-    images: [],
     sent: false,
-    itemInfoSent: false,
     itemFormData: itemFormData,
+    images: itemFormData.images,
     itemInfo: formatFormToItemInfo(itemFormData),
   };
 
   send.sendings[id] = shipment;
 
-  shipment.images = await Promise.all(
-    Object.keys(itemFormData.images).map(getNewImageByLocalUri),
-  );
+  async function getCompressedImageUri(image: Image) {
+    if (!image.localUriCompressed) {
+      try {
+        image.localUriCompressed = await compressImage(image.localUri);
+      } catch (err) {
+        console.error('error compressing image', err);
+        image.localUriCompressed = image.localUri;
+      }
+    }
+    return image;
+  }
+
+  await Promise.all(shipment.images.map(getCompressedImageUri));
 
   while (!shipment.sent) {
     try {
-      if (!shipment.itemInfoSent) {
+      if (!shipment.savedItem) {
         shipment.savedItem = await sendItemCreationRequest(shipment);
-        shipment.itemInfoSent = true;
         // console.error('item info saved', shipment.savedItem);
       } else {
         associateLocalAndRemoteImages(shipment);
