d64c7d09624e26647c4b90543e20868440708ac9
Ulisses <m.stunik@gmail.com>
2021-10-15 06:31:30 -0300

    change color

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -9,23 +9,25 @@ import {searchOptions} from 'search/searchOptions';
 import {LocationOption} from 'home/LocationOption';
 import {FooterNavigation} from 'navigation/FooterNavigation';
 import {SearchCustomHeader} from 'search/SearchCustomHeader';
+import {observe} from 'mobx';
 
 export function HomeScreen() {
   const [data, setData] = useState([]);
 
-  async function fetchProjects() {
+  async function getPlants() {
     const res = await api.get('plants/' + 1, formatSearch(searchOptions));
     setData(res.data);
     return res.data;
   }
 
   useEffect(() => {
-    fetchProjects();
+    observe(searchOptions, getPlants);
+    getPlants();
   }, []);
 
   return (
     <>
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         <SearchCustomHeader />
         <FlatList
           numColumns={2}
