ed911286ae48ea34de79c87406e6cd0bcd136eb4
Ulisses <m.stunik@gmail.com>
2021-10-16 11:56:09 -0300

    fix overlay press close modal

--- src/modal/ModalContext.tsx
+++ src/modal/ModalContext.tsx
@@ -14,6 +14,7 @@ export function ModalContextProvider({children}) {
   };
 
   const closeModal = () => {
+    console.error('aqui');
     setModalActive(false);
   };
 
--- src/modal/ModalDisplay.tsx
+++ src/modal/ModalDisplay.tsx
@@ -2,10 +2,10 @@ import React, {useEffect, useRef, useState} from 'react';
 import {useModal} from 'modal/ModalContext';
 import {Modalize} from 'react-native-modalize';
 import {StyleSheet} from 'react-native';
-import {useObserver} from 'mobx-react-lite';
+import {Observer, useObserver} from 'mobx-react-lite';
 
 export function ModalDisplay() {
-  const {currentModal, modalActive, modalOptions} = useModal();
+  const {currentModal, modalActive, closeModal, modalOptions} = useModal();
 
   const [showFloating, setShowFloating] = useState(true);
 
@@ -19,28 +19,33 @@ export function ModalDisplay() {
 
   const {FloatingComponent, onClosed, ...rest} = modalOptions;
 
-  return useObserver(() => (
-    <>
-      {modalActive && currentModal ? (
-        <Modalize
-          ref={ref}
-          onClose={() => setShowFloating(false)}
-          onClosed={e => {
-            setShowFloating(true);
-            if (onClosed) {
-              onClosed(e);
-            }
-          }}
-          snapPoint={modalOptions.snapPoint || 200}
-          handlePosition={'inside'}
-          FloatingComponent={showFloating && FloatingComponent}
-          handleStyle={styles.handle}
-          {...rest}>
-          {currentModal}
-        </Modalize>
-      ) : null}
-    </>
-  ));
+  return (
+    <Observer>
+      {() => (
+        <>
+          {modalActive && currentModal ? (
+            <Modalize
+              ref={ref}
+              onClose={() => setShowFloating(false)}
+              onClosed={e => {
+                setShowFloating(true);
+                if (onClosed) {
+                  onClosed(e);
+                }
+              }}
+              handlePosition={'inside'}
+              snapPoint={modalOptions.snapPoint || 200}
+              overlayStyle={{backgroundColor: '#0006'}}
+              FloatingComponent={showFloating && FloatingComponent}
+              handleStyle={styles.handle}
+              {...rest}>
+              {currentModal}
+            </Modalize>
+          ) : null}
+        </>
+      )}
+    </Observer>
+  );
 }
 
 const styles = StyleSheet.create({
