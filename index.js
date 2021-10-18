78673cd4ab731d9539b81769b86e48c22f3a3113
Ulisses <m.stunik@gmail.com>
2021-10-17 21:55:32 -0300

    first try

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ import {AppRegistry} from 'react-native';
 import App from './src/App.tsx';
 import {name as appName} from './app.json';
 
+import SplashScreen from 'react-native-splash-screen';
+
 AppRegistry.registerComponent(appName, () => App);
