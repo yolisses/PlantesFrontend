68e7bc3c57b40c5d067fe141cc60605900ab74a0
Ulisses <m.stunik@gmail.com>
2021-10-18 06:34:55 -0300

    use saved login in useUser

--- src/user/useUser.ts
+++ src/user/useUser.ts
@@ -1,8 +1,14 @@
-import {api} from 'api/api';
 import {useQuery} from 'react-query';
+
+import {api} from 'api/api';
+import {auth} from 'auth/auth';
 import {User} from 'types/User';
 
 async function getUser(userId) {
+  if (userId === auth?.user?.id) {
+    return auth?.user;
+  }
+
   try {
     const {data} = await api.get('users/' + userId);
     return data;
